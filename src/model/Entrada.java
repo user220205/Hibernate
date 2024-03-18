c491059120919591e45e2cc806fcd8ec4db571b0
Yanis <ydahmani@lanzaderaproyectos.com>
2024-03-18 18:43:33 +0100

    #1

--- src/model/Entrada.java
+++ src/model/Entrada.java
@@ -2,20 +2,36 @@ package model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name= "Entrada")
+@Table(name = "Entrada")
 public class Entrada {
-	@Id
-	@Column(name = "id")
-	private String valor;
 
-	@Column(name = "instruccion")
-	private String instruccion;
-	
-	public String getInstruccion() {
-		return this.instruccion;
-	}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "instruccion")
+    private String instruccion;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getInstruccion() {
+        return instruccion;
+    }
+
+    public void setInstruccion(String instruccion) {
+        this.instruccion = instruccion;
+    }
 }
